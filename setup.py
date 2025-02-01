9a0198186f9e342b8b5f3618e10ea2517de90bf7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-02-01 17:06:00 +0500

    feat: setup updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='styled_print',
-    version='0.1.0',  
+    version='0.1.1',  
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -10,7 +10,7 @@ setup(
     ],
     author='Usmonov Shoxruxmirzo',
     author_email='usmonovshohruxmirzo@gmail.com',
-    description='Pencil Art Generator from Images',
+    description='A flexible terminal text styling utility for color, background, and custom formatting options.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/webbro-software/styled_print',
