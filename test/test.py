c94286ca285490635cb9fb9111e1dcffd6d6ea11
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-02-01 17:16:32 +0500

    feat: v0.1.3

--- test/test.py
+++ test/test.py
@@ -1,16 +1,36 @@
 from styled_print import styled_print
 
-options = {
+
+styled_print("hello", {
     "color": "red",
     "styles": ["bold"],
     "bg_color": "on_white",
     "uppercase": True,
     "align": "center",
     "padding": 2,
+    "border": False
+})
+styled_print("hello", {
+    "color": "red",
+    "styles": ["bold"],
+    "bg_color": "on_red",
+    "uppercase": True,
+    "align": "center",
+    "padding": 2,
     "border": True
-}
-
-styled_print("hello", options)
-styled_print("hello", options)
-styled_print("hello", options)
-styled_print("hello", options)
+})
+styled_print("hello", {
+    "color": "red",
+    "styles": ["bold"],
+    "uppercase": True,
+    "align": "left",
+    "padding": 2,
+    "border": False
+})
+styled_print("hello", {
+    "color": "yellow",
+    "styles": ["underline"],
+    "uppercase": True,
+    "align": "left",
+    "border": False
+})
