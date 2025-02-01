a0b4449d9afc7a5b926d6224395c4cc8abd9fda3
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-02-01 16:50:41 +0500

    feat: core updated

--- src/styled_print/core.py
+++ src/styled_print/core.py
@@ -1,2 +1,9 @@
-def styled_print():
-    pass
+def styled_print(string, options):
+    color = options.get("color")
+    styles = options.get("styles", [])
+    bg_color = options.get("bg_color")
+    uppercase = options.get("uppercase", False)
+    lowercase = options.get("lowercase", False)
+    align = options.get("align", "left") 
+    padding = options.get("padding", 0)
+    border = options.get("border", False) 
